b3caf95e4da0c3ad2f20c0b83e8d096e5a2bf8a9
rvanata <vanata.regina@student.greenriver.edu>
2022-02-20 15:28:20 -0500

    added validation for forms

---
+++ model/validation.php
@@ -0,0 +1,66 @@
+<?php
+    function validChoices($userChoices){
+
+        //store the valid condiments
+        $choices = getChoices();
+
+        //check each selected condiment
+        foreach ($userChoices as $selection){
+            if(!in_array($selection, $choices)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    function validName($name){
+        if($name == ""){
+            return false;
+        }
+        return true;
+    }
+
+    function validAge($age){
+        if($age < 18 || $age > 118){
+            return false;
+        }
+        return true;
+    }
+
+    function validPhone($phone){
+        $isValid = true;
+        //if phone is less than 10 digits
+        //or more than 14 digits
+        //return false
+        //allow for + and country code
+        if (!empty($phone)) {
+            $phoneLength = strlen($phone);
+            if ($phoneLength < 10 || $phoneLength > 15) {
+                $isValid = false;
+            }
+            else{
+                if (!preg_match("/^[+]?[1-9][0-9]{9,14}$/", $phone)){
+                    $isValid = false;
+                }
+            }
+            return $isValid;
+        } else {
+            return false;
+        }
+    }
+
+    function validEmail($email){
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return false;
+        }
+        return true;
+    }
+
+    function validOutdoor($outdoorChoices){
+
+    }
+
+    function validIndoor($indoorChoices){
+
+    }
