53a8d00dd73efc44c150558ae0c8ad5c132b5925
rvanata <vanata.regina@student.greenriver.edu>
2022-03-06 06:15:09 -0500

    added params, constructors, getters, setters

--- classes/premiumMember.php
+++ classes/premiumMember.php
@@ -1,1 +1,53 @@
 <?php
+
+// 328/dating/premiumMember.php
+
+class PremiumMember extends Member
+{
+    private $_inDoorInterests;
+    private $_outDoorInterests;
+
+    /**
+     * @param $_inDoorInterests
+     * @param $_outDoorInterests
+     */
+    public function __construct($_inDoorInterests, $_outDoorInterests)
+    {
+        $this->_inDoorInterests = $_inDoorInterests;
+        $this->_outDoorInterests = $_outDoorInterests;
+    }
+
+    /**
+     * @return array
+     */
+    public function getInDoorInterests()
+    {
+        return $this->_inDoorInterests;
+    }
+
+    /**
+     * @param array $inDoorInterests
+     */
+    public function setInDoorInterests($inDoorInterests)
+    {
+        $this->_inDoorInterests = $inDoorInterests;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOutDoorInterests()
+    {
+        return $this->_outDoorInterests;
+    }
+
+    /**
+     * @param array $outDoorInterests
+     */
+    public function setOutDoorInterests($outDoorInterests)
+    {
+        $this->_outDoorInterests = $outDoorInterests;
+    }
+
+
+}
