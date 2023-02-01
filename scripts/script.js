28122efce56c515619f32e8adc8ee33386415e07
ptagliavia <79185768+ptagliavia@users.noreply.github.com>
2023-01-31 18:45:25 -0800

    Added print button and ctrl + p functionality and print media query styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 window.onload = function () {
     let button = document.getElementById("submitButton");
+    let printButton = document.getElementById("printButton");
+
     button.addEventListener("click", function() {
         alert("Plan Saved");
+    });
+
+    printButton.addEventListener("click", function() {
+        window.print();
     });
 }
